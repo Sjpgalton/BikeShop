db53126ff20ae2e178d9584b5808eb17e7766f41
Steven Galton <stevengaltonredweb@gmail.com>
2017-05-25 12:09:18 +0100

    Product Detail Page
    Product detail page has been added and this displays all the information on the page. This also has a breadcrumb trail

--- Redweb.BikeShop/Redweb.BikeShop/Controllers/ProductsController.cs
+++ Redweb.BikeShop/Redweb.BikeShop/Controllers/ProductsController.cs
@@ -1,6 +1,6 @@
 ﻿using Redweb.BikeShop.Core.Repositories;
-using System.Web.Mvc;
 using Redweb.BikeShop.Core.ViewModels;
+using System.Web.Mvc;
 
 namespace Redweb.BikeShop.Controllers
 {
@@ -13,11 +13,37 @@ namespace Redweb.BikeShop.Controllers
             _productRepository = productRepository;
         }
 
+        /// <summary>
+        /// Gets all the products.
+        /// </summary>
+        /// <returns>ViewResult.</returns>
         public ActionResult AllProducts()
         {
             var allProducts = _productRepository.GetAllProducts();
 
             return View(allProducts);
+        }
+
+        /// <summary>
+        /// Gets the Product details.
+        /// </summary>
+        /// <param name="productId">The product identifier.</param>
+        /// <returns>ViewResult.</returns>
+        public ActionResult ProductDetails(int id)
+        {
+            var productDetails = _productRepository.GetSingleProduct(id);
+
+            if (productDetails == null)
+                return HttpNotFound();
+
+            var viewModel = new ProductDetailViewModel
+            {
+                ProductDetail = productDetails,
+                ColourText = productDetails.Colour == null ? "N/A" : productDetails.Colour.Name,
+                SizeText = productDetails.Size == null ? "N/A" : productDetails.Size.Name
+            };
+
+            return View(viewModel);
         }
     }
 }
